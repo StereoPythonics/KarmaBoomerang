c4844370e791d8192b3fe9431a0088a77ab9ee34
TheLuckyCat42 <44896526+TheLuckyCat42@users.noreply.github.com>
2022-01-18 20:02:49 +0000

    Joel joins project
    Reorderes some assets, renames some stuff, adds dotween

---
+++ Assets/_Scripts/Player.cs
@@ -0,0 +1,42 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace KarmaBoomerang
+{
+    public class Player : MonoBehaviour
+    {
+        public float speed; //like max?
+        public Collider2D myCollider;
+        public bool isDead;
+
+        // Start is called before the first frame update
+        void Start()
+        {
+
+        }
+
+        private void Awake()
+        {
+
+        }
+
+        private void OnEnable()
+        {
+
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+
+        }
+
+        private void OnTriggerEnter2D(Collider2D collision)
+        {
+            
+        }
+    }
+}
+
